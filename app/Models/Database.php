ece2551c2b16b8ee8cb5ff597e8536eec828e2bb
Laggon <laggonroll@gmail.com>
2025-03-01 14:37:16 +0500

    Add Database model

---
+++ app/Models/Database.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Database extends Model
+{
+    protected $fillable = [
+        'name',
+        'server_id',
+    ];
+
+    public function server()
+    {
+        return $this->belongsTo(Server::class);
+    }
+}
--- app/Models/Server.php
+++ app/Models/Server.php
@@ -15,4 +15,9 @@ class Server extends Model
     {
         return $this->hasMany(Site::class);
     }
+
+    public function databases()
+    {
+        return $this->hasMany(Database::class);
+    }
 }
